8fbc8b320e6f6cfbf872549a53c1dc573e046e18
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-06-04 10:53:35 +0200

    init: react boilerplate

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+import 'semantic-ui-css/semantic.min.css'
+
+//import { /*boostrap-comp*/} from 'react-bootstrap'
+function App() {
+  return (
+    <div className="App">
+
+    </div>
+  );
+}
+
+export default App;
