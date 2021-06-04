d7644c5bac48bffb99c7a16f3a5caebba9f4c3b3
Maksym Sharin <Maksym.sharin.work@gmail.com>
2021-06-04 11:43:10 +0200

    iteration: navbar component

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,13 +1,16 @@
 
 import './App.css';
 import 'semantic-ui-css/semantic.min.css'
+import "bootstrap/dist/css/bootstrap.min.css";
+import  NavBar from "./components/Navbar"
+
 
 //import { /*boostrap-comp*/} from 'react-bootstrap'
 function App() {
   return (
-    <div className="App">
 
-    </div>
+      <NavBar/>
+
   );
 }
 
--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -4,3 +4,56 @@ Here just a transposition of Nav in react-bootstrap components
 Control Coordination
 by SearchBar and Search results
  */
+//Iteration 1:
+import {Component} from "react";
+import {Container, Navbar, Nav, NavDropdown} from 'react-bootstrap'
+
+class NavBar extends Component {
+    render() {
+        return (
+            <div>
+                <Navbar collapseOnSelect expand="lg" bg="dark" variant="dark">
+                    <Navbar.Brand href="#home">
+                        <img
+                            alt=""
+                            src="assets/netflix_logo.png"
+                            width="100"
+                            height="50"
+                            className="d-inline-block align-top"
+                        />
+                    </Navbar.Brand>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav"/>
+                    <Navbar.Collapse id="responsive-navbar-nav">
+
+                        <Nav className="me-auto">
+                            <Nav.Link href="#features">Features</Nav.Link>
+                            <Nav.Link href="#pricing">Pricing</Nav.Link>
+                            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
+                                <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
+                                <NavDropdown.Item href="#action/3.3">Something</NavDropdown.Item>
+                                <NavDropdown.Divider/>
+                                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
+                            </NavDropdown>
+                        </Nav>
+
+                        <Nav className={"ml-auto"}>
+                            <Nav.Link href="#deets">More deets</Nav.Link>
+                            <Nav.Link eventKey={2} href="#memes">
+                                Dank memes
+                            </Nav.Link>
+                        </Nav>
+
+                    </Navbar.Collapse>
+
+                </Navbar>
+
+                <br />
+
+            </div>
+
+
+        )
+    }
+}
+export default NavBar
